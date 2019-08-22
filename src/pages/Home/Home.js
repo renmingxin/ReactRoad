29713262ad870760b09c3c0a53c395afd98d218e
renmingxin <916586942@qq.com>
2019-08-22 20:47:56 +0800

    首页

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,18 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                首页
+                <header>发表话题</header>
+                <content>
+                    <div>
+                        <span>作者姓名:</span>
+                        <input type="text"/>
+                    </div>
+                    <div>
+                        <span>文章标题:</span>
+                        <input type="text"/>
+                    </div>
+                </content>
+                <button>提交</button>
             </div>
         )
     }
